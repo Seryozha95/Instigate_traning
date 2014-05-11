16b2bd9cccac7a04d1c26b1bd86d871b298428c9
Instigate_traning <serojkhachatryan95@gmail.com>
2014-05-12 00:45:45 +0400

    libraryBooks/

---
+++ libraryBooks/Reader.java
@@ -0,0 +1,13 @@
+import person.*;
+public class Reader extends Person {
+    public String card;
+    public int numberOfCard;
+    public Reader(String n, String sn, String psn, int noc) {
+        name=n;
+        surname=sn;
+        passportSerialNumber=psn;
+        numberOfCard=noc;
+    }
+}
+        
+
---
+++ libraryBooks/person/Person.java
@@ -0,0 +1,6 @@
+package person;
+public class Person {
+    public String name;
+    public String surname;
+    public String passportSerialNumber;
+}
