8b340fc263a00102ad0e88b91429df8a6fc9faf0
Instigate_traning <serojkhachatryan95@gmail.com>
2014-05-14 06:46:50 +0400

    erankyunBajanarar/

--- erankyunBajanarar/ErankyunTiv.java
+++ erankyunBajanarar/ErankyunTiv.java
@@ -3,12 +3,10 @@ public class ErankyunTiv {
     public int numberDivisor;
     public ErankyunTiv(){
     }
-    public int erankyunNumber(int number) {
+    public void erankyunNumber(int number) {
         numberValue = numberValue +number;
-        return numberValue;
     }
-    public int divisor(int numberValue, int i){
+    public void divisor(int numberValue, int i){
         numberDivisor=numberValue%i;
-        return numberDivisor;
     }
 }
--- erankyunBajanarar/Main.java
+++ erankyunBajanarar/Main.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 public class Main {
+    public void Main(){
+    }
+    public void MainFunction(ErankyunTiv myNumber, int quantityOfDivisor) {
+        for (int n =0; n < quantityOfDivisor;) {
+             for ( int number =0; ; number ++) {
+                 myNumber.erankyunNumber(number);
+                 for ( int i=1; i < myNumber.numberValue; i++) {
+                     myNumber.divisor(myNumber.numberValue, i);
+                     if (myNumber.numberDivisor == 0) {
+                         n++;
+                     }
+                     if (n ==  quantityOfDivisor) {
+                        System.out.println(myNumber.numberValue);
+                         return;
+                     }
+                 }
+                 n=0;
+             }
+        }
+    }
+
     public static void main(String args[]) {
         Scanner scan=new Scanner(System.in);
-        System.out.print("How many divisors do you want ? ");
+        System.out.println("How many divisors do you want ? ");
         int quantityOfDivisor=scan.nextInt();
         ErankyunTiv myNumber = new ErankyunTiv();
-//        while (true) {
-            for (int n =0; n < quantityOfDivisor;) {
-               for ( int number =0; ; number ++) {
-                    myNumber.erankyunNumber(number);
-                    System.out.println(myNumber.numberValue);
-                    for ( int i=1; i < myNumber.numberValue; i++) {
-                        myNumber.divisor(myNumber.numberValue, i);
-                        if (myNumber.numberDivisor == 0) {
-                               n++;
-                        }
-                        if (n ==  quantityOfDivisor) {
-                            break;
-                        }
-                        
-
-                    }
-                }
-            }
-        }
-  //  }
+        Main m=new Main();
+        m.MainFunction(myNumber,quantityOfDivisor);
+    }
 }
-    
