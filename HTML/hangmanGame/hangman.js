7dff6552d39fce5acede98e68a712a22b98898c3
Seryozha <serojkhachatryan95@mail.ru>
2014-05-22 15:57:16 +0400

    hangman

--- HTML/hangmanGame/hangman.js
+++ HTML/hangmanGame/hangman.js
@@ -1,25 +1,44 @@
-var word="porc";
-var correctGuesses="0";
-var placeholders=new Array
-function checkLetter() {
-    var newLetter=document.getElementById("input").value;
-    alert(newLetter);
-    for (var i = 0; i < word.length; i++) {
-        if (word.charAt(i) == newLetter) {
-            correctGuesses++;
-            alert(correctGuesses);
-           placeholders.push(document.getElementById("input").value);
-            if (correctGuesses == word.length)
-            { 
-                alert("verj");
-                return;
-            }
-        }
-        else {
-        //    alert(correctGuesses);
-          //  alert("no");
-        }
+var Question={
+   "ques" : [ "What is my name",
+   "What is your name"
+      ],
+   "answer" : [ "Seryozha",
+   "Naira"
+   ],
 
-    }
+};
+var randomNumber = Math.floor((Math.random() * 10));
+function playquation(){
+    var tr=document.getElementById("collom");
+       for (var i = 0; i < Question.answer[1].length; i++) {
+                 var td= document.createElement("td");
+                  
+                 tr.appendChild(td);
+       }
+
+   document.getElementById("main").style.display="block";
+   document.getElementById("printQuestion").innerHTML=Question.ques[1];
+}
+var correctGuesses="0";
+var placeholders=new Array();
+function checkLetter() {
+   var newLetter=document.getElementById("input").value;
+   alert(newLetter);
+
+   for (var i = 0; i < Question.answer[1].length; i++) {
+             if (Question.answer[1].charAt(i) == newLetter) {
+         correctGuesses++;
+         alert(correctGuesses);
+         placeholders.push(document.getElementById("input").value);
+         if (correctGuesses == word.length)
+         { 
+            alert("verj");
+            return;
+         }
+      }
+      else {
+      }
+
+   }
 }
 
