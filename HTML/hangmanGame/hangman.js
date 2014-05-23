aa4e8b672de6ab260cc74e8a2fc70b4ad084bbf1
Seryozha <serojkhachatryan95.com>
2014-05-23 08:36:21 +0400

    Hangman Game

--- HTML/hangmanGame/hangman.js
+++ HTML/hangmanGame/hangman.js
@@ -1,44 +1,57 @@
 var Question={
-   "ques" : [ "What is my name",
-   "What is your name"
-      ],
-   "answer" : [ "Seryozha",
-   "Naira"
-   ],
+    "ques" : [ "What is my name",
+    "What is your name"
+        ],
+    "answer" : [ "Seryozha",
+    "Naira"
+        ],
 
 };
-var randomNumber = Math.floor((Math.random() * 10));
+var randomNumber = Math.floor((Math.random() * Question.ques.length));
 function playquation(){
     var tr=document.getElementById("collom");
-       for (var i = 0; i < Question.answer[1].length; i++) {
-                 var td= document.createElement("td");
-                  
-                 tr.appendChild(td);
-       }
+    for (var i = 0; i < Question.answer[randomNumber].length; i++) {
+        var td= document.createElement("td");
+        td.setAttribute("id", "element" + i);
+        tr.appendChild(td);
+    }
 
-   document.getElementById("main").style.display="block";
-   document.getElementById("printQuestion").innerHTML=Question.ques[1];
+    document.getElementById("main").style.display="block";
+    document.getElementById("printQuestion").innerHTML=Question.ques[randomNumber];
 }
 var correctGuesses="0";
-var placeholders=new Array();
+var gameOver="0";
 function checkLetter() {
-   var newLetter=document.getElementById("input").value;
-   alert(newLetter);
+    var newLetter=document.getElementById("input").value;
+    if (document.getElementById("input").value !="") {
+        document.getElementById("input").value="";
+        var badGuesses="0";
+        
+        for (var i = 0; i < Question.answer[randomNumber].length; i++) {
+            if (Question.answer[randomNumber].charAt(i) == newLetter) {
+                correctGuesses++;
+                alert(correctGuesses);
+                if (correctGuesses == Question.answer[randomNumber].length)
+                { 
+                    alert("verj");
+                    return;
+                }
+            }
+            else {
+                badGuesses++;
+               // alert(badGuesses);
+                if (badGuesses == Question.answer[randomNumber].length)
+                {
+                    gameOver++;
+                    alert(gameOver);
+                    if (gameOver == 5) {
+                        alert();
+                    }
 
-   for (var i = 0; i < Question.answer[1].length; i++) {
-             if (Question.answer[1].charAt(i) == newLetter) {
-         correctGuesses++;
-         alert(correctGuesses);
-         placeholders.push(document.getElementById("input").value);
-         if (correctGuesses == word.length)
-         { 
-            alert("verj");
-            return;
-         }
-      }
-      else {
-      }
+                }
 
-   }
+            }
+        }
+    }
 }
 
