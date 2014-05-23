dfda2e0c0d05bb0fdd085be3a55c7002b49f3ca0
M7Arman <armankostandyan@gmail.com>
2014-05-23 16:20:24 +0400

    class

--- HTML/hangmanGame/js/hangman.js
+++ HTML/hangmanGame/js/hangman.js
@@ -1,78 +1,94 @@
-var Question={
-    "ques" : [ "A very huge animal","Which frut is red or green and rounded","The bigest country","The hotest continents"],
-    "answer" : [ "elephant","apple","Russia","Africa"],
-};
+var question = [
+{ "questionText": "A very huge animal",
+   "questionAnswer":"elephant"
+},
+
+{ "questionText":"Which frut is red or green and rounded",
+   "questionAnswer":"apple"
+},
+
+{
+   "questionText":"The bigest country",
+   "questionAnswer":"Russia"
+},
+
+{
+   "questionText":"The hotest continents",
+   "questionAnswer":"Africa"
+}
+
+];
 var inputLetter = document.getElementById("input");
-var randomNumber = Math.floor((Math.random() * Question.ques.length));
-currentWord = Question.answer[randomNumber];
+var randomNumber = Math.floor((Math.random() * question.length));
+currentWord = question[randomNumber].questionAnswer;
 var l = currentWord.length;
 
 
 function play(){
-	
-	this.style.display = "none";
-    var tr=document.getElementById("collom");
-    for (var i = 0; i < l; i++) {
-        var td= document.createElement("td");
-        td.setAttribute("id", "element" + i);
-        tr.appendChild(td);
-    }
 
-    document.getElementById("main").style.display="block";
-    document.getElementById("printQuestion").innerHTML=Question.ques[randomNumber];
+   this.style.display = "none";
+   var tr=document.getElementById("collom");
+   for (var i = 0; i < l; i++) {
+      var td= document.createElement("td");
+      td.setAttribute("id", "element" + i);
+      tr.appendChild(td);
+   }
+
+   document.getElementById("main").style.display="block";
+   document.getElementById("printQuestion").innerHTML=question[randomNumber].questionText;
 }
 var correctGuesses="0";
 var badGuessCount="0";
 function checkLetter() {
-    var newLetter=document.getElementById("input").value;
-    if (document.getElementById("input").value !="") {
-        var badGuesses="0";
-        inputLetter.value = "";
-        for (var i = 0; i < l; i++) {
-            if (currentWord[i].toLowerCase() == newLetter.toLowerCase()) {
-                correctGuesses++;
-				
-				
-				var letter = document.createElement("span");
-				letter.innerHTML = currentWord[i];
-				document.getElementById("element"+i).appendChild(letter);
-				
-				currentWord = currentWord.replace(currentWord[i],"0");
-				
-                if (correctGuesses == l)
-                { 
-                    gameOver();
-                    alert("WIN!!!");
-                }
-            }
-            else {
-                badGuesses++;
-                if (badGuesses == l)
-                {
-                    badGuessCount++;
-                    if (badGuessCount == 5) {
-						gameOver();
-                        alert("Game Over");
-                    }
+   var newLetter=document.getElementById("input").value;
+   if (document.getElementById("input").value !="") {
+      var badGuesses="0";
+      inputLetter.value = "";
+      for (var i = 0; i < l; i++) {
+         if (currentWord[i].toLowerCase() == newLetter.toLowerCase()) {
+            correctGuesses++;
 
-                }
+
+            var letter = document.createElement("span");
+            letter.innerHTML = currentWord[i];
+            document.getElementById("element"+i).appendChild(letter);
+
+            currentWord = currentWord.replace(currentWord[i],"0");
+
+            if (correctGuesses == l)
+            { 
+               gameOver();
+               alert("WIN!!!");
+            }
+         }
+         else {
+            badGuesses++;
+            if (badGuesses == l)
+            {
+               badGuessCount++;
+               if (badGuessCount == 5) {
+                  gameOver();
+                  alert("Game Over");
+               }
 
             }
-        }
-    }
-	
+
+         }
+      }
+   }
+
 }
 
 inputLetter.onclick = function() {
-	this.value="";
+   this.value="";
 }
 
 document.getElementById("play").onclick = play;
 
 
 function gameOver(){
-	inputLetter.setAttribute("disabled","");
-	
+   inputLetter.setAttribute("disabled","");
+
 } 
 
 
