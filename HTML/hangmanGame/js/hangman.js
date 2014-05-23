849c1a5658c8ca499d296d48edfca4451a19708e
DavidInstegate <ghdavidc@yahoo.com>
2014-05-23 14:28:20 +0400

    HTML/hangmanGame/

---
+++ HTML/hangmanGame/js/hangman.js
@@ -0,0 +1,90 @@
+var Question={
+    "ques" : [ "A very huge animal","Which frut is red or green and rounded","The bigest country","The hotest continents"],
+    "answer" : [ "elephant","apple","Russia","Africa"],
+};
+var inputLetter = document.getElementById("input");
+var randomNumber = Math.floor((Math.random() * Question.ques.length));
+currentWord = Question.answer[randomNumber];
+var l = currentWord.length;
+
+
+function play(){
+	
+	this.style.display = "none";
+    var tr=document.getElementById("collom");
+    for (var i = 0; i < l; i++) {
+        var td= document.createElement("td");
+        td.setAttribute("id", "element" + i);
+        tr.appendChild(td);
+    }
+
+    document.getElementById("main").style.display="block";
+    document.getElementById("printQuestion").innerHTML=Question.ques[randomNumber];
+}
+var correctGuesses="0";
+var badGuessCount="0";
+function checkLetter() {
+    var newLetter=document.getElementById("input").value;
+    if (document.getElementById("input").value !="") {
+        var badGuesses="0";
+        inputLetter.value = "";
+        for (var i = 0; i < l; i++) {
+            if (currentWord[i].toLowerCase() == newLetter.toLowerCase()) {
+                correctGuesses++;
+				
+				
+				var letter = document.createElement("span");
+				letter.innerHTML = currentWord[i];
+				document.getElementById("element"+i).appendChild(letter);
+				
+				currentWord = currentWord.replace(currentWord[i],"0");
+				
+                if (correctGuesses == l)
+                { 
+                    gameOver();
+                    alert("WIN!!!");
+                }
+            }
+            else {
+                badGuesses++;
+                if (badGuesses == l)
+                {
+                    badGuessCount++;
+                    if (badGuessCount == 5) {
+						gameOver();
+                        alert("Game Over");
+                    }
+
+                }
+
+            }
+        }
+    }
+	
+}
+
+inputLetter.onclick = function() {
+	this.value="";
+}
+
+document.getElementById("play").onclick = play;
+
+
+function gameOver(){
+	inputLetter.setAttribute("disabled","");
+	
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
