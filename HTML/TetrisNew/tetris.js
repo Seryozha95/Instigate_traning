13053e78eddf40dd8b6a525d153e87442ebff912
Seryozha <serojkhachatryan95@mail.ru>
2014-06-04 17:46:28 +0400

    Move figure tetris

--- HTML/TetrisNew/tetris.js
+++ HTML/TetrisNew/tetris.js
@@ -4,16 +4,16 @@ var figureCordinates = [
 {
 	"figure":[
 	{ 
-		cord:["190","30", squareHeight,squareWith],
+		cord:[190,30, squareHeight,squareWith],
 	},
 	{
-		cord:["210","30", squareHeight,squareWith],
+		cord:[210,30, squareHeight,squareWith],
 	},
 	{
-		cord:["190" ,"10", squareHeight, squareWith],
+		cord:[190 ,10, squareHeight, squareWith],
 	},
 	{
-		cord:["210" ,"10", squareHeight, squareWith],
+		cord:[210 ,10, squareHeight, squareWith],
 	},
 	
 	],
@@ -30,28 +30,36 @@ function play() {
 */
 var drowCanvas = document.getElementById("drowCanvas"),
     drowCanvas = drowCanvas.getContext('2d');
+var figureNumber = Math.floor((Math.random() * figureCordinates.length));
 function init() {
     
     table= new canvasTable(50,50,300,500);
 	console.log(table.xCord);
-    drowCanvas.strokeRect(table.xCoord, table.yCoord, table.newHeight, table.newWith);
+  drowCanvas.strokeRect(table.xCoord, table.yCoord, table.newHeight, table.newWith);
      }
-function refresh() {
-/*
-kubik1 =new canvasFigure (figureCordinates[0].squareOne[0], figureCordinates[0].squareOne[1], 10, 10);
-kubik2 =new canvasFigure (figureCordinates[0].squareTwo[0], figureCordinates[0].squareTwo[1], 10, 10);
-kubik3 =new canvasFigure (figureCordinates[0].squareTree[0], figureCordinates[0].squareTree[1], 10, 10);
-kubik4 =new canvasFigure (figureCordinates[0].squareFour[0], figureCordinates[0].squareFour[1], 10, 10);
-drowCanvas.fillRect(kubik1.xCoord,kubik1.yCoord, squareHeight,squareWith );
-drowCanvas.fillRect(kubik2.xCoord,kubik2.yCoord, squareHeight,squareWith );
-drowCanvas.fillRect(kubik3.xCoord,kubik3.yCoord, squareHeight,squareWith );
-drowCanvas.fillRect(kubik4.xCoord,kubik4.yCoord, squareHeight,squareWith );
-*/
-
+function createFigure() {
+//	drowCanvas.beginPath();
 	for ( var i =0; i<4; i++) {
-	drowCanvas.fillRect(figureCordinates[0].figure[i].cord[0],figureCordinates[0].figure[i].cord[1], squareHeight, squareWith);
-
+	drowCanvas.fillRect(figureCordinates[figureNumber].figure[i].cord[0],figureCordinates[figureNumber].figure[i].cord[1], squareHeight, squareWith);
 	}
+}
+console.log(figureCordinates[0].figure[0].cord[1] + 10);
+console.log(figureCordinates[0].figure[0].cord[1] + 10);
+;
+var moveYCord=0;
+function moveFigure(){
+	
+	
+	moveYCord+=60;
+	drowCanvas.beginPath();
+		for ( var i =0; i<4; i++) {
+		
+	drowCanvas.fillRect( figureCordinates[figureNumber].figure[i].cord[0],figureCordinates[figureNumber].figure[i].cord[1]+moveYCord,squareHeight, squareWith);
+	
+	
+	}
+
+
 }
 function canvasTable (xCoord, yCoord, newHeight, newWith) {
       this.xCoord=xCoord;
@@ -62,11 +70,5 @@ function canvasTable (xCoord, yCoord, newHeight, newWith) {
 }
 
 
-/*function canvasFigure(xCoord, yCoord) {
-      this.xCoord=xCoord;
-      this.yCoord=yCoord;
-
-}
-    */
     
      
