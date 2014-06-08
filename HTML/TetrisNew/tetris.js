06c05e23d45f2845b56ee990dc95122407964cee
Seryozha <serojkhachatryan95.com>
2014-06-08 13:23:31 +0400

    Clear figure all

--- HTML/TetrisNew/tetris.js
+++ HTML/TetrisNew/tetris.js
@@ -1,9 +1,10 @@
-var squareHeight=10;
-var squareWith=10;
-var keycode;
-var moveYCord=0;
-var moveXCord=0;
-
+var squareHeight = 10;
+var squareWith = 10;
+var keycode = 0;
+var moveYCord = 0;
+var moveXCord = 0;
+var carentYCoord = new Array();
+var carentXCoord = new Array();
 var figureCordinates = [
 {
 	"figure":[
@@ -22,6 +23,22 @@ var figureCordinates = [
 	
 	],
 },
+/*{
+	"figure":[
+	{
+		cord:[ ],
+	},
+	{
+		cord:[ ],
+	},
+	{
+		cord:[ ],
+	},
+	{
+		cord:[ ],
+	},
+	],
+},*/
 	];
 
 
@@ -37,66 +54,64 @@ function play() {
 var Canvas = document.getElementById("myCanvas");
 var drowCanvas = Canvas.getContext('2d');
 var figureNumber = Math.floor((Math.random() * figureCordinates.length));
-var count=figureCordinates[figureNumber].figure.length;
-//document.onkeydown = function checkKeycode(event);
+var count = figureCordinates[figureNumber].figure.length;
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function init() {
-      table= new canvasTable(60,50,300,500);
-	  drowCanvas.strokeRect(table.xCoord, table.yCoord, table.newHeight, table.newWith);
-	setInterval(moveFigure, 600);     
-     }
+	table = new canvasTable(60, 50, 300, 500);
+	drowCanvas.strokeRect(table.xCoord, table.yCoord, table.newHeight, table.newWith);
+	setInterval(moveFigure, 1000);
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////     
 function createFigure() {
-	for ( var i =0; i<count; i++) {
-	drowCanvas.fillRect(figureCordinates[figureNumber].figure[i].cord[0],figureCordinates[figureNumber].figure[i].cord[1], squareHeight, squareWith);
+	for ( var i = 0; i < count; i++) {
+	drowCanvas.fillRect(figureCordinates[figureNumber].figure[i].cord[0], figureCordinates[figureNumber].figure[i].cord[1], squareHeight, squareWith);
 	}
 }
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-document.onkeydown = function checkKeyCode(event)
-{      
-	 if(event.which) 
+document.onkeydown = function checkKeyCode(event){      
+	if(event.which) 
     	keycode = event.which;
-    if(keycode==37){
-    	moveXCord=moveXCord-squareHeight;
-    	moveYCord-=squareHeight;
-	     	moveFigure();
+    if(keycode == 37) {
+    	moveXCord = moveXCord - squareHeight;
+    	moveYCord -= squareHeight;
+	   	moveFigure();
      }
-    if(keycode==39){
-    	moveXCord=moveXCord+squareHeight;
-    	moveYCord-=squareHeight;
+    if(keycode == 39) {
+    	moveXCord = moveXCord + squareHeight;
+    	moveYCord -= squareHeight;
      	moveFigure();
     }
- }
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function moveFigure(){
-	moveYCord+=squareHeight;
-	drowCanvas.beginPath();
-		drowCanvas.clearRect(60,45,300,490);
-		for ( var i =0; i<count; i++) {
-			drowCanvas.fillRect(figureCordinates[figureNumber].figure[i].cord[0]+moveXCord,figureCordinates[figureNumber].figure[i].cord[1]+moveYCord,squareHeight, squareWith);
-		}
-		drowCanvas.closePath();
-		drowCanvas.fill();
+function moveFigure() {
+	moveYCord += squareHeight;
+	drowCanvas.fillStyle = '#000';
+	for ( var i = 0; i < count; i++) {
+		drowCanvas.clearRect(carentXCoord[i], carentYCoord[i], squareHeight, squareWith);	
 	}
-
+	for ( var i = 0; i < count; i++) {
+		carentXCoord[i] = (figureCordinates[figureNumber].figure[i].cord[0]+moveXCord);
+		carentYCoord[i] = (figureCordinates[figureNumber].figure[i].cord[1]+moveYCord);
+		drowCanvas.fillRect(carentXCoord[i], carentYCoord[i], squareHeight, squareWith);
+	}
+}
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function canvasTable (xCoord, yCoord, newHeight, newWith) {
-      this.xCoord=xCoord;
-      this.yCoord=yCoord;
-      this.newHeight=newHeight;
-      this.newWith=newWith;
-       
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+    this.newHeight = newHeight;
+    this.newWith = newWith;
 }
 
 
-     
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
